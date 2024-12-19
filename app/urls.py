1f119a877eed517027005f8290e4c962dbb52746
thaonp <tnguyenphuong276@gmail.com>
2024-12-19 14:54:36 +0700

    refacter(appmodul): remove add  modul

--- app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.home, name="home"),
-    path('service/', views.service, name="service"),
-    path('register/', views.register, name="register"),
-]
